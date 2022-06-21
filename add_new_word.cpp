c4ebbf19a5e706a644bbe94b427c779d45352c45
nghiemkythu <nghiemdt2013@gmail.com>
2022-06-21 21:12:16 +0700

    fix sth

--- add_new_word.cpp
+++ add_new_word.cpp
@@ -1,6 +1,6 @@
 using namespace std;
 #include "Trie.h"
-
+#include "add_new_word.h"
 //root_key for trie saving key
 //root_def for trie saving definition of key
 void add_new_word(TrieNode* &root_key, TrieNode* &root_def, string s, string meaning){
