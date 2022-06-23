d18e4cdf42cec9e65c980e2756bce0fbdb1c77e3
trnthienphc2003 <trnthienphc2003@gmail.com>
2022-06-23 09:51:14 +0700

    Small fixes

--- Trie.h
+++ Trie.h
@@ -6,28 +6,30 @@
 #include <fstream>
 
 using namespace std;
+// alphabet: ASCII 32-127 (all printable characters)
+// For Dictionary project (with special characters like -, $, #...)
 const int ALP=96;
 
 struct TrieNode {
     int cnt;
     TrieNode *c[ALP];
     bool isEndOfWord;
-    string meaning;
+    vector <string> meaning;
 
     TrieNode() {
         cnt=0;
         for(int i=0; i<ALP; ++i)
             c[i]=nullptr;
         isEndOfWord=false;
+        meaning={};
     }
 };
 
 void insert(TrieNode *&root, string s, string meaning);
-bool lookUpMeaning1(TrieNode* root, string s, string& meaning);
-bool EditDefinition(TrieNode*& root, string s, string change);
+// bool lookUpMeaning1(TrieNode* root, string s, string& meaning);
 void traverse(vector <pair<string, string>> &v, TrieNode* root, string s);
 vector <pair<string, string>> lookUpMeaning(TrieNode *root, string s);
-void Deallocate(TrieNode* &root);
+void deallocate(TrieNode* &root);
 bool isEmpty(TrieNode* root);
 TrieNode* remove(TrieNode* &root, string key, int len);
 void deleteHistory();
