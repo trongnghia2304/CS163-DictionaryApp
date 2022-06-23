42ddc297b7403b692de7cf3e6f7e68f5cd4434b9
nghiemkythu <nghiemdt2013@gmail.com>
2022-06-23 09:28:56 +0700

    small fix

--- Trie.h
+++ Trie.h
@@ -24,7 +24,7 @@ struct TrieNode {
 
 void insert(TrieNode *&root, string s, string meaning);
 bool lookUpMeaning1(TrieNode* root, string s, string& meaning);
-bool EditDefination(TrieNode*& root, string s, string change);
+bool EditDefinition(TrieNode*& root, string s, string change);
 void traverse(vector <pair<string, string>> &v, TrieNode* root, string s);
 vector <pair<string, string>> lookUpMeaning(TrieNode *root, string s);
 void Deallocate(TrieNode* &root);
