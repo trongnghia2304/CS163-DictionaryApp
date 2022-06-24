e0e8911e0b6092b2629f6d7f00406b41949ddb82
trnthienphc2003 <trnthienphc2003@gmail.com>
2022-06-24 09:53:05 +0700

    Small fixes

--- splitTu.h
+++ splitTu.h
@@ -1,5 +1,9 @@
 #include <vector>
 #include <string>
+#include <cstring>
+#include <sstream>
+#include <algorithm>
+#include <bits/stdc++.h>
 
 std::vector<std::string> split(std::string str, std::string delimiter);
 std::vector < std::string> cumTu(std::string str, int limit);
