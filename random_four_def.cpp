93ea2a897c0cf047e22ab360bd0505631b21f098
nghiemkythu <nghiemdt2013@gmail.com>
2022-06-28 14:53:22 +0700

    finish function random four def(12)

--- random_four_def.cpp
+++ random_four_def.cpp
@@ -5,12 +5,13 @@ using namespace std;
 #include "random_four_def.h"
 #include "random_word.h"
 
-void random_key_4_def(TrieNode* root_key, TrieNode* root_def, string &s, vector<string> &meaning){
+void random_key_4_def(TrieNode* root_key, TrieNode* root_def, string &s, vector<string> &meaning, string &right_meaning){
     vector<string>mean;
     random_word(root_key, s, mean);
     srand(time(NULL)); 
 	int res = rand() % (mean.size());
     meaning.push_back(mean[res]);
+    right_meaning = mean[res];
     while(meaning.size() < 4){
         string new_def;
         vector<string> new_mean;
--- random_four_def.h
+++ random_four_def.h
@@ -1,4 +1,4 @@
 #pragma once
 #include "Trie.h"
 
-void random_key_4_def(TrieNode* root_key, TrieNode* root_def, string &s, vector<string> &meaning);
+void random_key_4_def(TrieNode* root_key, TrieNode* root_def, string &s, vector<string> &meaning,  string &right_meaning);
