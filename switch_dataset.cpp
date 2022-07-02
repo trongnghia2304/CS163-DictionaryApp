65befe9907ca77584ad7f3f5ebe7edec32dcf978
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2022-07-02 21:43:13 +0700

    complement to task 1
    alternating build function and adding reset function

--- switch_dataset.cpp
+++ switch_dataset.cpp
@@ -1,6 +1,6 @@
 using namespace std;
-#include "Trie.h"
-#include "add_new_word.h"
+#include "switch_dataset.h"
+
 //mode will be assigned one of 3 values: 
 // 1: Anh - Viet
 // 2: Viet - Anh
@@ -12,9 +12,9 @@ using namespace std;
 // sample3.txt
 // 
 //And the format of the txt file will be:
-// Nghiem'Hello/ con ga no minh 11k va di chan rau CT1821 PTNK
+// Nghiem'Hello/con ga no minh 11k va di chan rau CT1821 PTNK
 // Nghiem'Hello
-// data'definition
+// data'definition1/definition2/definition3/...
 
 void build_data(TrieNode*& root_key, TrieNode*& root_def, int mode)
 {
@@ -22,15 +22,56 @@ void build_data(TrieNode*& root_key, TrieNode*& root_def, int mode)
 
 	string s = "sample" + to_string(mode) + ".txt";
 	fin.open(s);
-    
-    string meaning;
-    string s;
+
+    string s, meaning;
     while (getline(fin, s, '\''))
     {
-        getline(fin, meaning, '\n');
+        getline(fin, meaning);
+        int len = meaning.size() - 1;
+        string str = "";
+        for (int i = 0; i <= len; i++)
+        {
+            if (meaning[i] == '/')
+            {
+                add_new_word(root_key, root_def, s, str);
+                str = "";
+            }
+            else str = str + meaning[i];
+        }
 
-        add_new_word(root_key, root_def, s, meaning);
+        add_new_word(root_key, root_def, s, str);
+    }
+    fin.close();
+}
+
+// 3 files which are the original datasets and used to recover the very first data:
+// origin1.txt
+// origin2.txt
+// origin3.txt
+
+// this function called when we press reset button
+void reset_file(int mode)
+{
+    ifstream fin;
+    ofstream fout;
+
+    string s = "origin" + to_string(mode) + ".txt";
+    string s1 = "sample" + to_string(mode) + ".txt";
+
+    fin.open(s);
+    fout.open(s1);
+    
+    while (getline(fin, s))
+    {
+        fout << s << endl;
     }
 
     fin.close();
+    fout.close();
+}
+
+//this function called to write down the 
+void write_changes(int mode)
+{
+
 }
--- switch_dataset.h
+++ switch_dataset.h
@@ -1,3 +1,6 @@
 #include "Trie.h"
+#include "add_new_word.h"
 
 void build_data(TrieNode*& root_key, TrieNode*& root_def, int mode);
+void reset_file(int mode);
+void write_changes(int mode);
