16a859c95e3f4e39ccb4bdfe255a925af27097c2
trnthienphc2003 <trnthienphc2003@gmail.com>
2022-07-03 15:35:21 +0700

    Create SystemData.h

---
+++ SystemData.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+const std::string Dictionary[]={"Emotional/", "Eng-Eng/", "Eng-Vie/", "Vie-Eng/", "Slang/"};
+const std::string AccessFile[]={"original.txt", "current.txt", "history.txt", "favorite.txt"};
