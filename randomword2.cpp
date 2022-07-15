179e93ea1b86045d0752365c4e279629ddedf80e
DucHungGithub <100983291+DucHungGithub@users.noreply.github.com>
2022-07-15 19:11:16 +0700

    random for games after modify

---
+++ randomword2.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+#include "randomword2.h"
+
+void random_word2(TrieNode*& root_key, string& s, vector<string>& meaning, int t) {
+    TrieNode* cur = root_key;
+    if (t == 1) srand(time(NULL));
+    int res = rand() % (96);
+    while (!cur->c[res]) {
+        res = rand() % (96);
+    }
+    cur = cur->c[res];
+    s = s + char(res + 32);
+    while (true) {
+        if (isEmpty(cur)) {
+            meaning = cur->meaning;
+            break;
+        }
+        else {
+            if (cur->isEndOfWord) {
+                int g = rand() % (2);
+                if (g == 1) {
+                    meaning = cur->meaning;
+                    break;
+                }
+                else {
+                    res = rand() % (96);
+                    while (!cur->c[res]) {
+                        res = rand() % (96);
+                    }
+                    cur = cur->c[res];
+                    s = s + char(res + 32);
+                }
+            }
+            else {
+                res = rand() % (96);
+                while (!cur->c[res]) {
+                    res = rand() % (96);
+                }
+                cur = cur->c[res];
+                s = s + char(res + 32);
+            }
+        }
+    }
+}
---
+++ randomword2.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "Trie2.h"
+
+void random_word2(TrieNode*& root_key, string& s, vector<string>& meaning, int t = 1);
